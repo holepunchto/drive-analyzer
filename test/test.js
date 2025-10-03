43369c8509e21398d80c1ee14bc7988a53798516
rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
2025-10-03 20:17:16 +0200

    return warmup and defer (#11)

--- test/test.js
+++ test/test.js
@@ -24,8 +24,8 @@ test('should generate map of esm app', async (t) => {
   const analyzer = new DriveAnalyzer(drive)
   analyzer.ready()
 
-  const encoded = await analyzer.analyze(['app.js'])
-  const decoded = DriveAnalyzer.decode(encoded.meta, encoded.data)
+  const { warmup } = await analyzer.analyze(['app.js'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
 
   t.ok(decoded.data.length !== 0)
   t.ok(decoded.meta.length !== 0)
@@ -51,8 +51,8 @@ test('should generate map of cjs app', async (t) => {
   const analyzer = new DriveAnalyzer(drive)
   analyzer.ready()
 
-  const encoded = await analyzer.analyze(['app.js'])
-  const decoded = DriveAnalyzer.decode(encoded.meta, encoded.data)
+  const { warmup } = await analyzer.analyze(['app.js'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
 
   t.ok(decoded.data.length !== 0)
   t.ok(decoded.meta.length !== 0)
@@ -78,8 +78,8 @@ test('preload asset', async (t) => {
   const analyzer = new DriveAnalyzer(drive)
   analyzer.ready()
 
-  const encoded = await analyzer.analyze(['app.js'], ['/assets/asset.txt'])
-  const decoded = DriveAnalyzer.decode(encoded.meta, encoded.data)
+  const { warmup } = await analyzer.analyze(['app.js'], ['/assets/asset.txt'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
 
   t.ok(decoded.data.length !== 0)
   t.ok(decoded.meta.length !== 0)
@@ -105,8 +105,8 @@ test('html entrypoint', async (t) => {
   const analyzer = new DriveAnalyzer(drive)
   analyzer.ready()
 
-  const encoded = await analyzer.analyze(['index.html'])
-  const decoded = DriveAnalyzer.decode(encoded.meta, encoded.data)
+  const { warmup } = await analyzer.analyze(['index.html'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
 
   t.ok(decoded.data.length !== 0)
   t.ok(decoded.meta.length !== 0)
@@ -132,8 +132,8 @@ test('html entrypoint in subdir', async (t) => {
   const analyzer = new DriveAnalyzer(drive)
   analyzer.ready()
 
-  const encoded = await analyzer.analyze(['/src/index.html'])
-  const decoded = DriveAnalyzer.decode(encoded.meta, encoded.data)
+  const { warmup } = await analyzer.analyze(['/src/index.html'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
 
   t.ok(decoded.data.length !== 0)
   t.ok(decoded.meta.length !== 0)
@@ -159,11 +159,71 @@ test('preload folder', async (t) => {
   const analyzer = new DriveAnalyzer(drive)
   analyzer.ready()
 
-  const encoded = await analyzer.analyze([], ['/assets'])
-  const decoded = DriveAnalyzer.decode(encoded.meta, encoded.data)
+  const { warmup } = await analyzer.analyze([], ['/assets'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
 
   t.ok(decoded.data.length !== 0)
   t.ok(decoded.meta.length !== 0)
+
+  await drive.close()
+  await store.close()
+})
+
+test('should skip module not found', async (t) => {
+  const storage = await getTmpDir(t)
+  const store = new Corestore(storage)
+  await store.ready()
+
+  const app = path.join(__dirname, 'fixtures', 'skips')
+  const localdrive = new Localdrive(app)
+  const drive = new Hyperdrive(store.session())
+  await localdrive.ready()
+  await drive.ready()
+
+  const mirror = new Mirrordrive(localdrive, drive)
+  await mirror.done()
+
+  const analyzer = new DriveAnalyzer(drive)
+  analyzer.ready()
+
+  const { warmup, skips } = await analyzer.analyze(['app.js'])
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
+
+  t.ok(decoded.data.length !== 0)
+  t.ok(decoded.meta.length !== 0)
+  t.is(skips.length, 1)
+  t.is(skips[0].specifier, 'bar')
+  t.is(skips[0].referrer.href, 'drive:///sub-dep.js')
+
+  await drive.close()
+  await store.close()
+})
+
+test('should defer skip', async (t) => {
+  const storage = await getTmpDir(t)
+  const store = new Corestore(storage)
+  await store.ready()
+
+  const app = path.join(__dirname, 'fixtures', 'skips')
+  const localdrive = new Localdrive(app)
+  const drive = new Hyperdrive(store.session())
+  await localdrive.ready()
+  await drive.ready()
+
+  const mirror = new Mirrordrive(localdrive, drive)
+  await mirror.done()
+
+  const analyzer = new DriveAnalyzer(drive)
+  analyzer.ready()
+
+  const { warmup, skips } = await analyzer.analyze(['app.js'], [], {
+    defer: ['bar']
+  })
+  const decoded = DriveAnalyzer.decode(warmup.meta, warmup.data)
+
+  t.ok(decoded.data.length !== 0)
+  t.ok(decoded.meta.length !== 0)
+  t.is(skips.length, 0)
 
   await drive.close()
   await store.close()
